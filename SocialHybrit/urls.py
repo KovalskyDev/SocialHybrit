06ed4270e314283e0eb3a9d7494e245aa44a5cd0
Вова Дубинка <dubinkavovchik406@gmail.com>
2026-02-15 18:50:38 +0200

    feat(MainPage): auth-crud and role-model

--- SocialHybrit/urls.py
+++ SocialHybrit/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("posts/", include("MainPage.urls"))
+    path("", include("MainPage.urls"))
     
 ]
