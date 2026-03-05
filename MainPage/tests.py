46cb6246b08eb71be9886237c7dffad94187a9e2
Вова Дубинка <dubinkavovchik406@gmail.com>
2026-03-05 20:17:24 +0200

    feat(MainPage): tests funk working

--- MainPage/tests.py
+++ MainPage/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Post
+class PostLstTest(TestCase):
+    def test_post_list_access(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
-# Create your tests here.
+    def test_api_post_list(self):
+        response = self.client.get("/api/posts/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_post(self):
+        post = Post.objects.create(
+            name = "ggg",
+            about = "jjj"
+        )
+        self.assertEqual(post.name, "ggg")
+        self.assertEqual(post.about, "jjj")
