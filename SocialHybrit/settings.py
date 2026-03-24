bd38846384da236c304c1c54a42a34189d5027ac
Вова Дубинка <dubinkavovchik406@gmail.com>
2026-03-24 13:14:17 +0200

    Configure prod branch with SQLite and migrated data

--- SocialHybrit/settings.py
+++ SocialHybrit/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 
 # Application definition
@@ -106,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
 ]
